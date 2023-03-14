9d1c6a1fe6cc6b5aca27e25a4c3ab116173a4ad2
Aakanksha Malwade <aakanksha.m20@gmail.com>
2023-03-14 18:18:22 +0530

    Add files via upload
    C++ Program to find quotient and remainder.

---
+++ Quotient_Remainder.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int dividend, divisor, qoutient, remainder;
+     
+    cout << "Enter dividend: ";
+    cin >> dividend;
+
+    cout << "Enter divisor: ";
+    cin >> divisor;
+
+    qoutient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Quotient = " << qoutient << endl;
+    cout << "Remainder = " << remainder;
+
+
+    return 0;
+}
